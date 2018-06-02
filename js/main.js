54de32200ba6e8fa89e539f5a9197cd1de783633
Trin3232 <emanuel6464@mail.com>
2018-06-02 19:31:57 +0100

    Update 20
    Updated Wordpress Theme name to from m.e theme to Ypip Theme.
    
    Updated Images folder with x2 new stock photos for Glasgow & Edinburgh. Also renamed, one photo file in Leigh Buzz folder.
    
    Updated all html pages with onclick function to dropdown button for accommad.html page updated JS Main file with corresponding code.
    
    Updated Accommad.html markup for (a tag) buttons links & updated CSS
    
    Updated link tags order on all html pages
    
    Delete M.E Theme.zip folder

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ console.log('it works');
 
 var d = new Date();
 document.getElementById("update-year-text").innerHTML = d.getFullYear();
+
+// On click event to change Accommodation in Navbar to a Link
+
+function gotoaccommad(){
+  window.location.href="accommad.html";
+}
