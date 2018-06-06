ca9529584d28e80597e0e49ac2dfc6c39f6f147b
Trin3232 <emanuel6464@mail.com>
2018-06-06 19:05:45 +0100

    Update 20.5
    Created & updated freebies html page and added markup & images
    
    Updated alt image info on images tags
    
    Updated CSS
    
    Added x3 new images to image folder being used for Freebies html page
    
    Started Jquery code for animate in JS file

--- js/main.js
+++ js/main.js
@@ -10,3 +10,26 @@ document.getElementById("update-year-text").innerHTML = d.getFullYear();
 function gotoaccommad(){
   window.location.href="accommad.html";
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var head = document.getElementByID("head");
+
+// Get the offset position of the navbar
+var sticky = head.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    head.classList.add("sticky")
+  } else {
+    head.classList.remove("sticky");
+  }
+}
+
+
+// Animate Freebies Images
+
+$('.break-fist').addClass('animated bounceIn');
