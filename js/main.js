049409f60b1e730654031009d3a5067448298a92
Trin3232 <emanuel6464@mail.com>
2018-06-29 15:11:51 +0100

    Update 30.8.5
    Updated contact.html page contact div layout
    
    Updated CSS & Removed broken JS Code

--- js/main.js
+++ js/main.js
@@ -9,40 +9,11 @@ document.getElementById("update-year-text").innerHTML = d.getFullYear();
 
 function gotoaccommad(){
   window.location.href="accommad.html";
-
 }
 
 function gotoservices(){
   window.location.href="services.html";
-
 }
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var head = document.getElementByID("head");
-
-// Get the offset position of the navbar
-var sticky = head.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    head.classList.add("sticky")
-  } else {
-    head.classList.remove("sticky");
-  }
-}
-
-
-// Animate Freebies Images
-
-
-// $(function() {
-//
-//   $('.').on('mouseover', function() {
-//     $('#break-fist').addClass('animated bounceIn');
-//   });
-//
-// });
